1dc6a13b2a89e75e34e64da518c14f79765c759f
Deepakraj M <deepakraj.m@shield.com>
2024-10-30 18:54:34 +0530

    version updated to 1.5.46

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "1-5-45"
-let shieldfraud_checksum = "365779ca0c86821196ed2f060baea0bab0e4048156157183523ebf1f0c6f190a"
+let version = "1-5-46"
+let shieldfraud_checksum = "da6b0bfd815aa1a64775ebef4f279677f607f4d791eb0b3550b5d93746e106ec"
 
 let package = Package(
     name: "shieldfraud-spm",
